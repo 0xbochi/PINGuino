550c5aaa77268437c93779ca8c3dc059efbb6fe6
Luckas BOSCH <boschluckas@gmail.com>
2020-05-28 17:42:29 +0200

    Add project files.

---
+++ pingerMonitor/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace pingerMonitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lb_ping = new System.Windows.Forms.Label();
+            this.lb_srv = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(67, 57);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lb_ping
+            // 
+            this.lb_ping.AutoSize = true;
+            this.lb_ping.Font = new System.Drawing.Font("Arial", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_ping.ForeColor = System.Drawing.Color.White;
+            this.lb_ping.Location = new System.Drawing.Point(28, 76);
+            this.lb_ping.Name = "lb_ping";
+            this.lb_ping.Size = new System.Drawing.Size(96, 32);
+            this.lb_ping.TabIndex = 1;
+            this.lb_ping.Text = "Ping : ";
+            // 
+            // lb_srv
+            // 
+            this.lb_srv.AutoSize = true;
+            this.lb_srv.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_srv.ForeColor = System.Drawing.Color.White;
+            this.lb_srv.Location = new System.Drawing.Point(74, 25);
+            this.lb_srv.Name = "lb_srv";
+            this.lb_srv.Size = new System.Drawing.Size(108, 16);
+            this.lb_srv.TabIndex = 3;
+            this.lb_srv.Text = "Server : Google";
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.chart1.BorderlineColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            chartArea1.BackColor = System.Drawing.Color.Transparent;
+            chartArea1.CursorY.Interval = 5D;
+            chartArea1.Name = "ChartArea1";
+            chartArea1.ShadowColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(-102, 111);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Bright;
+            series1.BackImageTransparentColor = System.Drawing.Color.Transparent;
+            series1.BackSecondaryColor = System.Drawing.Color.Transparent;
+            series1.BorderColor = System.Drawing.Color.Transparent;
+            series1.BorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.NotSet;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series1.Color = System.Drawing.Color.White;
+            series1.Font = new System.Drawing.Font("Microsoft Sans Serif", 1.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            series1.LabelBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            series1.LabelBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            series1.LabelForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            series1.Legend = "Legend1";
+            series1.MarkerBorderColor = System.Drawing.Color.White;
+            series1.MarkerColor = System.Drawing.Color.White;
+            series1.Name = "ping";
+            series1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Grayscale;
+            series1.ShadowColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(965, 175);
+            this.chart1.TabIndex = 7;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.ClientSize = new System.Drawing.Size(703, 260);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.lb_srv);
+            this.Controls.Add(this.lb_ping);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "PINGguin";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lb_ping;
+        private System.Windows.Forms.Label lb_srv;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ pingerMonitor/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace pingerMonitor
+{
+
+
+
+    public partial class Form1 : Form
+    {
+
+        public int seconds { get; set; }
+        public int ping { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string pingHost(string host)
+        {
+            System.Net.NetworkInformation.Ping pinger = new Ping();
+            PingReply reply = pinger.Send(host);
+
+            return (reply.RoundtripTime).ToString();
+
+
+        }
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //lb_sync_ping.Text = pingHost("8.8.8.8");
+            chart1.ChartAreas[0].AxisX.MajorGrid.Enabled = false;
+            chart1.ChartAreas[0].AxisX.MinorGrid.Enabled = false;
+            chart1.ChartAreas[0].AxisY.MajorGrid.Enabled = false;
+            chart1.ChartAreas[0].AxisY.MinorGrid.Enabled = false;
+
+            chart1.ChartAreas[0].AxisX.Enabled = AxisEnabled.False;
+            chart1.ChartAreas[0].AxisY.Enabled = AxisEnabled.False;
+
+            chart1.Legends.Clear();
+
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            string ping = pingHost("8.8.8.8");
+            lb_ping.Text = "Ping : " + ping + " MS";
+            chart1.Series["ping"].Points.AddY(ping);
+
+            int colorPing = Convert.ToInt32(ping);
+
+            if (colorPing <= 25)
+            {
+                lb_ping.ForeColor = System.Drawing.Color.White;
+            }
+            if (colorPing > 25 && colorPing < 55)
+            {
+                lb_ping.ForeColor = System.Drawing.Color.Green;
+            }
+
+            if (colorPing > 55 && colorPing < 80)
+            {
+                lb_ping.ForeColor = System.Drawing.Color.Brown;
+            }
+
+            if (colorPing > 80)
+            {
+                lb_ping.ForeColor = System.Drawing.Color.Red;
+            }
+
+
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //lb_sync_ping.Text = pingHost("8.8.8.8");
+        }
+
+        private void Lb_sync_ping_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
